4cd4b23d5d68090b2e352308e76ef08448f142ae
vinhphat <vinhphat@live.fr>
2022-05-24 10:52:21 +0200

    fixmongo

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const DB_URI = process.env.DB_URI;
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(DB_URI);
+mongoose.connect(MONGO_URL);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
